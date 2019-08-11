2bd058c3b7dbf3a8b37219009923051930c11216
shen_wei <nuaasw@hotmail.com>
2019-08-11 21:07:59 +0800

    工时数据可视化分析

--- base.py
+++ base.py
@@ -1,17 +1,15 @@
-# import matplotlib.pyplot as plt
-# import numpy
-#
-# datas = [1,4,9,16,25]
-# print(datas)
-# plt.plot(list(range(1,6)),datas)
-# plt.show()
-# print('End')
-
+import matplotlib.pyplot as plt
 import numpy
 import csv
 
-print('Hello Test!')
+datas = [1,4,9,16,25]
+# print(datas)
 
+
+print('Hello Test!')
+normalTime = {}
+overTime = {}
+allTime = {}
 filename = 'C:/Users/Administrator/PycharmProjects/dataanalysis/data/test.csv'
 with open(filename,'r',encoding='UTF-8') as f:
     reader = csv.reader(f)
@@ -20,3 +18,24 @@ with open(filename,'r',encoding='UTF-8') as f:
     for index,value in enumerate(head_row):
         ivalue[index] = value
     print(ivalue)
+    # 正常工时
+    for row in reader:
+        if row[10] == '按员工汇总':
+            # 获取员工编号
+            userid = row[2]
+            normalTime[userid] = float(row[13])
+            overTime[userid] = float(row[14])
+            allTime[userid] = float(row[13])+float(row[14])
+    # print(normalTime)
+# print(normalTime.keys())
+x = list(range(len(normalTime.keys())))
+print(x)
+print(normalTime.values())
+
+plt.plot(x,list(normalTime.values()))
+plt.scatter(x,list(normalTime.values()),s=10)
+plt.plot(x,list(overTime.values()))
+plt.scatter(x,list(overTime.values()),s=10)
+plt.plot(x,list(allTime.values()))
+plt.scatter(x,list(allTime.values()),s=30)
+plt.show()
