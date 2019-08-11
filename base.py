5896738f0c1f7282a2b7db6f08f461ef136426bd
shen_wei <nuaasw@hotmail.com>
2019-08-11 19:45:50 +0800

    读写csv文件

--- base.py
+++ base.py
@@ -1,8 +1,22 @@
-import matplotlib.pyplot as plt
-import numpy
+# import matplotlib.pyplot as plt
+# import numpy
+#
+# datas = [1,4,9,16,25]
+# print(datas)
+# plt.plot(list(range(1,6)),datas)
+# plt.show()
+# print('End')
 
-datas = [1,4,9,16,25]
-print(datas)
-plt.plot(list(range(1,6)),datas)
-plt.show()
-print('End')
+import numpy
+import csv
+
+print('Hello Test!')
+
+filename = 'C:/Users/Administrator/PycharmProjects/dataanalysis/data/test.csv'
+with open(filename,'r',encoding='UTF-8') as f:
+    reader = csv.reader(f)
+    head_row = next(reader)
+    ivalue = {}
+    for index,value in enumerate(head_row):
+        ivalue[index] = value
+    print(ivalue)
